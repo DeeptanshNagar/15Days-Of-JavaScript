666ddc2e19000b15e786062fc0e8b55ff3b47fe8
Diptansh <diptanshnagar@gmail.com>
2024-12-16 02:30:20 +0530

    js24

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Diptansh");
+
+
+let name = "Diptansh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => same as let state;
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
