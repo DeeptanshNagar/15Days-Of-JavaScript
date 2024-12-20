a88e11ae37ac19e6fed97c10af098548ba8edf09
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-21 04:05:40 +0530

    date

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,29 @@
+// Date : Date is an object in javascript. 
+// JavaScript stores dates as number of milliseconds since January 01, 1970
+const myDate = new Date();
+// console.log(myDate) //Output : 2023-10-09T14:38:09.847Z
+// We can use various methods to convert this date into more readable value.
+// console.log(myDate.toString()) //Output : Mon Oct 09 2023 07:39:18 GMT-0700 (Pacific Daylight Time)
+
+// console.log(myDate.toLocaleString()) // Output : 10/9/2023, 7:44:20 AM
+// console.log(myDate.toLocaleDateString()) //Output : 10/9/2023
+// console.log(myDate.toLocaleTimeString()) // Output : 7:45:31 AM
+
+// console.log(myDate.toDateString()) //Output : Mon Oct 09 2023
+// console.log(myDate.toTimeString()) // Output : 07:46:40 GMT-0700 (Pacific Daylight Time)
+
+// console.log(myDate.toISOString()) // Output : 2023-10-09T14:43:39.337Z
+// console.log(myDate.toJSON()) //Output : 2023-10-09T14:40:58.495Z
+
+// console.log(typeof myDate) //Output : object;
+
+let myCreatedDate = new Date(2023, 0, 23);
+// console.log(myCreatedDate) //Output : 2023-01-22T18:30:00.000Z;
+// console.log(myCreatedDate.toString()) //Mon Jan 23 2023 00:00:00 GMT+0530 (India Standard Time);
+
+let myCreatedDate1 = new Date(2023, 0, 23, 12, 30, 30);
+console.log(myCreatedDate1.toLocaleString()); // 23/1/2023, 12:30:30 pm
+// let myCreatedDate1 = new Date("2023-01-14");
+// console.log(myCreatedDate1.toLocaleString()); // 14/1/2023, 5:30:00 am
+// let myCreatedDate1 = new Date("01-14-2023");
+// console.log(myCreatedDate1.toLocaleString()); // 1/14/2023, 12:00:00 am
